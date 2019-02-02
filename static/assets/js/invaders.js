9eab1cf99472be59a904e98cbde2a2d186c4021b
brian-quach <briiquach@gmail.com>
2019-02-02 17:29:09 -0500

    Shooting works - yay

--- static/assets/js/invaders.js
+++ static/assets/js/invaders.js
@@ -6,14 +6,20 @@ let config = {
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 200 }
+            gravity: { y: 0 }
         }
     },
     scene: {
         preload: preload,
-        create: create
+        create: create,
+        update: update
     }
 };
+
+let cursors;
+let fireButton;
+let player;
+let bullets;
 
 let game = new Phaser.Game(config);
 
@@ -22,20 +28,60 @@ function preload ()
     this.load.setBaseURL('http://labs.phaser.io');
 
     this.load.image('sky', 'assets/skies/space3.png');
-    game.load.image('ship', 'assets/games/invaders/player.png');
+    this.load.image('ship', 'assets/sprites/ship.png');
+    this.load.image('bullet', 'assets/sprites/poo.png', 20, 20);
 }
 
 function create ()
 {
+    // Background
     this.add.image(400, 300, 'sky');
 
+    // Player
+    player = this.physics.add.sprite(100, 450, 'ship');
+    player.setCollideWorldBounds(true);
 
-    //  The hero!
-    player = game.add.sprite(400, 500, 'ship');
-    player.anchor.setTo(0.5, 0.5);
-    game.physics.enable(player, Phaser.Physics.ARCADE);
+    // Player Bullets
+    bullets = this.physics.add.group();
+
+    // Control Listeners
+    cursors = this.input.keyboard.createCursorKeys();
+    fireButton = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
 }
+
+let bulletDelay = 0;
 
 function update ()
 {
+    let moveSpeed = 200;
+
+    if (cursors.left.isDown) {
+        player.setVelocityX(-moveSpeed);
+    } else if (cursors.right.isDown) {
+        player.setVelocityX(moveSpeed);
+    //} else if (cursors.up.isDown) {
+    //    player.setVelocityY(-moveSpeed);
+    //} else if (cursors.down.isDown) {
+    //    player.setVelocityY(moveSpeed);
+    } else {
+        player.setVelocityX(0);
+    }
+
+    if(fireButton.isDown){
+        if(bulletDelay === 0){
+            fireBullet();
+            bulletDelay = 10;
+        } else {
+            bulletDelay--;
+        }
+    }
+}
+
+function fireBullet(){
+    console.log("FIRE");
+    let bullet = bullets.create(player.x, player.y, 'bullet');
+    bullet.setVelocityY(-500);
+    //bullet.setCollideWorldBounds(true);
+    bullet.outOfBoundsKill = true;
+
 }

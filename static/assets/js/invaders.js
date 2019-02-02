a7dce63b4341b1cd6dcfae20267b7a07f11ae5ab
brian-quach <briiquach@gmail.com>
2019-02-02 16:24:55 -0500

    12assa

--- static/assets/js/invaders.js
+++ static/assets/js/invaders.js
@@ -22,11 +22,18 @@ function preload ()
     this.load.setBaseURL('http://labs.phaser.io');
 
     this.load.image('sky', 'assets/skies/space3.png');
+    game.load.image('ship', 'assets/games/invaders/player.png');
 }
 
 function create ()
 {
     this.add.image(400, 300, 'sky');
+
+
+    //  The hero!
+    player = game.add.sprite(400, 500, 'ship');
+    player.anchor.setTo(0.5, 0.5);
+    game.physics.enable(player, Phaser.Physics.ARCADE);
 }
 
 function update ()

1c9acb5f3e62143daae808df904cf8443e698505
Jordan Ruetz <JordanR.9813@gmail.com>
2019-02-02 21:32:36 -0500

    writing backend hosting with azure

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express = require('express');
+/*const express = require('express');
 const app = express();
 
 const bodyParser = require('body-parser');
@@ -19,4 +19,30 @@ app.use(function (req, res, next){
 const PORT = 3000;
 app.listen(PORT, () =>{
     console.log("Started server on port", PORT);
-});
+});*/
+
+var http = require('http');
+var fs = require('fs');
+
+var port = process.env.PORT || 1337;
+
+var server = http.createServer(function(request, response) {
+	fs.readFile('static/index.html', function (err, data){
+		if (err) {
+			console.log("ERROR")
+			console.log(err)
+		} else {
+			response.writeHead(200, {'Content-Type': 'text/html'});
+        	response.write(data);
+        	response.end();
+		}
+    });
+
+
+/*    response.writeHead(200, {"Content-Type": "text/html"});
+    response.end("<p>Hello World!</p><p>hiiiii</p>");*/
+
+}).listen(port);
+
+
+console.log("Server running at http://localhost:%d", port);
